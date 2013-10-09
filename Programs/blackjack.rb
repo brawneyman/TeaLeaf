9512825900231acc7924ac3d322cdaa41771535f
Braun Bailey <braun.bailey@octanner.com>
2013-10-09 13:58:07 -0600

    Change Push

--- Programs/blackjack.rb
+++ Programs/blackjack.rb
@@ -1,20 +1,31 @@
-puts 'Welcome to Blackjack!'
-
+#Welcome to Blackjack
 
 def calculate_total(cards)
-# [['H', '3'], ['S', 'Q'], ... ]
+  # [['H', '3'], ['S', 'Q'], ... ]
+  arr = cards.map{|e| e[1] }
 
+  total = 0
+  arr.each do |value|
+    if value == "A"
+      total += 11
+    elsif value.to_i == 0 # J, Q, K
+      total += 10
+    else
+      total += value.to_i
+    end
+  end
+
+  #correct for Aces
+  arr.select{|e| e == "A"}.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
 end
 
-puts 'Welcome to Blackjack!'
-puts 'What is your Name?'
-my_name = gets.chomp
+# Start Game
 
-#spades = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-#hearts = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-#clubs = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-#diamonds = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-#deck = spades + hearts + clubs + diamonds
+puts "Welcome to Blackjack!"
 
 suits = ['H', 'D', 'S', 'C']
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -22,13 +33,15 @@ cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 deck = suits.product(cards)
 deck.shuffle!
 
+# Deal Cards
+
 mycards = []
 dealercards = []
 
-2.times do
-  mycards << deck.pop
-  dealercards << deck.pop
-end
+mycards << deck.pop
+dealercards << deck.pop
+mycards << deck.pop
+dealercards << deck.pop
 
 dealertotal = calculate_total(dealercards)
 mytotal = calculate_total(mycards)
@@ -38,11 +51,149 @@ mytotal = calculate_total(mycards)
 puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
 puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of: #{mytotal}"
 puts ""
-puts "What would you like to do? 1) hit 2) stay"
-hit_or_stay = gets.chomp
 
-#puts player_cards
-#puts '---'
-#puts dealer_cards
-#puts '---'
-#puts deck
+# Player turn
+if mytotal == 21
+  puts "Congratulations, you hit blackjack! You win!"
+  exit
+end
+
+while mytotal < 21
+  puts "What would you like to do? 1) hit 2) stay"
+  hit_or_stay = gets.chomp
+
+  if !['1', '2'].include?(hit_or_stay)
+    puts "Error: you must enter 1 or 2"
+    next
+  end
+
+  if hit_or_stay == "2"
+    puts "You chose to stay."
+    break
+  end
+
+  #hit
+  new_card = deck.pop
+  puts "Dealing card to player: #{new_card}"
+  mycards << new_card
+  mytotal = calculate_total(mycards)
+  puts "Your total is now: #{mytotal}"
+
+  if mytotal == 21
+    puts "Congratulations, you hit blackjack! You win!"
+    exit
+  elsif mytotal > 21
+    puts "Sorry, it looks like you busted!"
+    exit
+  end
+end
+
+# Dealer turn
+
+if dealertotal == 21
+  puts "Sorry, dealer hit blackjack. You lose."
+  exit
+end
+
+while dealertotal < 17
+  #hit
+  new_card = deck.pop
+  puts "Dealing new card for dealer: #{new_card}"
+  dealercards << new_card
+  dealertotal = calculate_total(dealercards)
+  puts "Dealer total is now: #{dealertotal}"
+
+  if dealertotal == 21
+    puts "Sorry, dealer hit blackjack. You lose."
+    exit
+  elsif dealertotal > 21
+    puts "Congratulations, dealer busted! You win!"
+    exit
+  end
+end
+
+# Compare hands
+
+puts "Dealer's cards: "
+dealercards.each do |card|
+  puts "=> #{card}"
+end
+puts ""
+
+puts "Your cards:"
+mycards.each do |card|
+  puts "=> #{card}"
+end
+puts ""
+
+if dealertotal > mytotal
+  puts "Sorry, dealer wins."
+elsif dealertotal < mytotal
+  puts "Congratulations, you win!"
+else
+  puts "It's a tie!"
+end
+
+exit
+
+
+#puts 'Welcome to Blackjack!'
+#
+#
+#def calculate_total(cards)
+## [['H', '3'], ['S', 'Q'], ... ]
+#  arr = cards.map{|e| e[1] }
+#
+#  total = 0
+#  arr.each do |value|
+#    if value == "A"
+#      total += 11
+#    elsif value.to_i == 0 # J, Q, K
+#      total += 10
+#    else
+#      total += value.to_i
+#    end
+#  end
+#
+#  #correct for Aces
+#  arr.select{|e| e == "A"}.count.times do
+#    total -= 10 if total > 21
+#  end
+#
+#  total
+#end
+#
+#puts 'Welcome to Blackjack!'
+#puts 'What is your Name?'
+#my_name = gets.chomp
+#
+#suits = ['H', 'D', 'S', 'C']
+#cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+#
+#deck = suits.product(cards)
+#deck.shuffle!
+#
+#mycards = []
+#dealercards = []
+#
+#2.times do
+#  mycards << deck.pop
+#  dealercards << deck.pop
+#end
+#
+#dealertotal = calculate_total(dealercards)
+#mytotal = calculate_total(mycards)
+#
+## Show Cards
+#
+#puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
+#puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of: #{mytotal}"
+#puts ""
+#puts "What would you like to do? 1) hit 2) stay"
+#hit_or_stay = gets.chomp
+#
+##puts player_cards
+##puts '---'
+##puts dealer_cards
+##puts '---'
+##puts deck
